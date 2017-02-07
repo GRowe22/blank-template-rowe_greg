821e89914e9d02ea1fe848cd62663edb9c97e724
Greg Rowe <growe22@ufl.edu>
2017-02-07 17:12:39 -0600

    add responsive grid system HTML

--- index.php
+++ index.php
@@ -2,15 +2,33 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title></title>
-    <!-- Links to our Style.css file -->
+    <title>Greg Rowe Blank Template</title>
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
 </head>
 
 <body>
 
-    <h1>Greg Rowe Blank WordPress Template</h1>
-    <p>This is my WordPress template.</p>
+    <div class="container">
+        <header class="row">
+            <div class="twelve columns">
+                <h1>Greg Rowe Blank WordPress Template</h1>
+                <p>This is my WordPress template.</p>
+            </div>
+        </header>
+
+        <section class="row">
+            <div class="twelve columns">
+                <h2>Section Header</h2>
+                <p>Section content goes here.</p>
+            </div>
+        </section>
+
+        <footer class="row">
+            <div class="twelve columns">
+                <p>Footer content goes here.</p>
+            </div>
+        </footer>
+    </div>
 
 </body>
 

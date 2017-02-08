19599dce9a3e068057ccfc742e556bdcc504521f
Greg Rowe <growe22@ufl.edu>
2017-02-07 19:48:34 -0600

    seperate template files

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Greg Rowe Blank Template</title>
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+</head>
+
+<body>
+
+    <div class="container"><!-- Begin Container Div-->
+<header class="row">
+    <div class="twelve columns">
+        <h1>Greg Rowe Blank WordPress Template</h1>
+        <p>This is my WordPress template.</p>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -1,35 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Greg Rowe Blank Template</title>
-    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
-</head>
+<?php get_header(); ?>
 
-<body>
-
-    <div class="container">
-        <header class="row">
-            <div class="twelve columns">
-                <h1>Greg Rowe Blank WordPress Template</h1>
-                <p>This is my WordPress template.</p>
-            </div>
-        </header>
-
-        <section class="row">
-            <div class="twelve columns">
-                <h2>Section Header</h2>
-                <p>Section content goes here.</p>
-            </div>
-        </section>
-
-        <footer class="row">
-            <div class="twelve columns">
-                <p>Footer content goes here.</p>
-            </div>
-        </footer>
+<!-- Begin Section Container -->
+<section class="row">
+    <div class="twelve columns">
+        <h2>Section Header</h2>
+        <p>Section content goes here.</p>
     </div>
+</section>
+<!-- End Section Container -->
 
-</body>
-
-</html>
+<?php get_footer(); ?>

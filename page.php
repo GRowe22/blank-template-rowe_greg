590f1b0d8a64d416c64dbb8dcef884a6780be2e4
Greg Rowe <growe22@ufl.edu>
2017-02-07 22:33:02 -0600

    add page content and php loop

--- page.php
+++ page.php
@@ -1,12 +1,15 @@
 <?php get_header(); ?>
+
 	<section class="row">
         <div class="twelve columns">
 
 <!-- BEGIN PAGE PHP -->
             <?php if (have_posts()) {
                 while (have_posts()) {
+                    /*OUR DATA CONTEXT IS DEFINED */
                     the_post(); ?>
-                    <h2><?php the_title(); ?></h2>
+
+                    <h3><?php the_title(); ?></h3>
                     <?php the_content();
                 }
             } ?>
@@ -14,5 +17,5 @@
 
         </div>
     </section>
-    
+
 <?php get_footer(); ?>

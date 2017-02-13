4ee9810b35846154ba23bbb9a0f61fcc5202a3cf
Greg Rowe <growe22@ufl.edu>
2017-02-13 16:57:39 -0600

    correct search errors

--- search.php
+++ search.php
@@ -11,11 +11,11 @@ get_header(); ?>
                     '<span>' . get_search_query() . '</span>' );
                 ?>
             </h1>
-
-            <?php if (have_posts()) :
-                while (have_posts()) : the_post(); ?> 						<h2><?php the_title(); ?></h2>
-            <?php the_content();
-                endwhile;
+            <?php 
+            while (have_posts()) : the_post(); ?>
+                <h2><?php the_title(); ?></h2>
+                    <?php the_content();
+            endwhile;
         else : ?>
             <h1>Nothing Found</h1>
             <p>Sorry, but nothing matched your search criteria. Please try again with different search terms.</p>

19489ba597d82a292dbda15e5d5c3b6981b97cb7
Greg Rowe <growe22@ufl.edu>
2017-02-13 16:22:36 -0600

    search php code and criteria

--- header.php
+++ header.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>Greg Rowe Blank Template</title>
-    <?php wp_head(); ?>
-    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+<title></title>
+<?php wp_head(); ?>
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
 </head>
 
 <body>
 
-    <div class="container"><!-- Begin Container Div-->
-<header class="row">
-    <div class="twelve columns">
-        <h1><?php bloginfo('name'); ?></h1>
-        <h2><?php bloginfo('description'); ?><h2>
-    </div>
-</header>
+<div class="container">
+    <header class="row">
+        <div class="nine columns">
+            <h1>Blank WordPress Template</h1>
+            <p>This is my WordPress template.</p>
+        </div>
+    <!-- Add Search Form -->
+        <div class="three columns">
+            <?php get_search_form(); ?>
+        </div>
+    </header>
 
-<!-- Add Menu Here -->
-<div class="row">
-    <div class="twelve columns">
-        <?php wp_nav_menu(array(
-            'sort_column' => 'menu_order',
-            'container_class' => 'blank-menu-header'
-            ));?>
+    <!-- Add Menu Here -->
+    <div class="row">
+        <div class="twelve columns">
+            <?php wp_nav_menu(array(
+                'sort_column' => 'menu_order',
+                'container_class' => 'blank-menu-header'
+                ));?>
+        </div>
     </div>
-</div>

a10c14ad891736c67f652c04f410d21811ef70ef
Greg Rowe <growe22@ufl.edu>
2017-02-13 18:47:55 -0600

    add permalink to post title

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
             if ( have_posts() ) {
                 while ( have_posts() ) {
                     the_post(); ?>
-
-                    <h3><?php the_title(); ?></h3>
+                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                     <?php
                     if ( has_post_thumbnail() ) {
                         the_post_thumbnail('thumbnail');

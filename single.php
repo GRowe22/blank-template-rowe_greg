40e74d0b62e80aa358947bbec86032ddc1603f89
Greg Rowe <growe22@ufl.edu>
2017-02-13 18:31:49 -0600

    edit single page code layout

--- single.php
+++ single.php
@@ -1,15 +1,16 @@
 <?php get_header(); ?>
-    <div class="row">
+    <section class="row">
         <div class="twelve columns">
-            <?php if (have_posts()) :
-                while (have_posts()) : the_post();
-                if ( has_post_thumbnail() ) { ?>
+            <?php
+                if (have_posts()) :
+                    while (have_posts()) : the_post();
+                    if ( has_post_thumbnail() ) { ?>
                     <div class="post-thumbnail"><?php the_post_thumbnail('large'); ?></div>
                 <?php } ?>
                     <h2><?php the_title(); ?></h2>
                     <?php the_content();
-                endwhile;
-            endif; ?>
+                    endwhile;
+                endif; ?>
         </div>
-    </div>
+    </section>
 <?php get_footer(); ?>
